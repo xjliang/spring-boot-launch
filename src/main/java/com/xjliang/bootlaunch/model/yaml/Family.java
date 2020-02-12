2212c9ee3817ae8fbdafb5d47b36db0b1348cc51
Xiaojian Liang <gnulxj@gmail.com>
2020-02-12 16:03:03 +0800

    3-4 配置文件注入值数据校验

--- src/main/java/com/xjliang/bootlaunch/model/yaml/Family.java
+++ src/main/java/com/xjliang/bootlaunch/model/yaml/Family.java
@@ -1,15 +1,19 @@
 package com.xjliang.bootlaunch.model.yaml;
 
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Data
 @Component
 @ConfigurationProperties(prefix = "family")
+@Validated
 public class Family {
 
     //@Value("${family.family-name}")
+    @NotEmpty
     private String familyName;
 
     private Father father;
