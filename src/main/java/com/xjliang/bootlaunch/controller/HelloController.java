70718b3f749405f453dbbd782166cda24e2119bf
Xiaojian Liang <gnulxj@gmail.com>
2020-02-14 23:08:41 +0800

    8-3 全局异常处理 ExceptionHandler

--- src/main/java/com/xjliang/bootlaunch/controller/HelloController.java
+++ src/main/java/com/xjliang/bootlaunch/controller/HelloController.java
@@ -1,13 +1,20 @@
 package com.xjliang.bootlaunch.controller;
 
+import com.xjliang.bootlaunch.config.exception.AjaxResponse;
 import com.xjliang.bootlaunch.model.ArticleVO;
+import com.xjliang.bootlaunch.service.ExceptionService;
+import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 public class HelloController {
+
+    @Resource
+    private ExceptionService exceptionService;
 
 //    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
 
@@ -17,5 +24,21 @@ public class HelloController {
         ArticleVO article = ArticleVO.builder().id(3L).author("无情").build();
         log.info("test: " + article);
         return article;
+    }
+
+    @RequestMapping("/ex/system")
+    public @ResponseBody
+    AjaxResponse system() {
+
+        exceptionService.systemBizError();
+
+        return AjaxResponse.success();
+    }
+
+
+    @RequestMapping("/ex/user")
+    public @ResponseBody  AjaxResponse user(Integer input) {
+
+        return AjaxResponse.success(exceptionService.userBizError(input));
     }
 }
