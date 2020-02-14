9f50b739ae3cb86a42d13f8b86900490f04fccb9
Xiaojian Liang <gnulxj@gmail.com>
2020-02-14 23:03:00 +0800

    8-2 自定义异常和相关数据结构

--- src/main/java/com/xjliang/bootlaunch/config/exception/AjaxResponse.java
+++ src/main/java/com/xjliang/bootlaunch/config/exception/AjaxResponse.java
@@ -1,4 +1,4 @@
-package com.xjliang.bootlaunch.model;
+package com.xjliang.bootlaunch.config.exception;
 
 
 import io.swagger.annotations.ApiModel;
@@ -20,6 +20,22 @@ public class AjaxResponse {
 
     private AjaxResponse() {
 
+    }
+
+    //请求出现异常时的响应数据封装
+    public static AjaxResponse error(CustomException e) {
+
+        AjaxResponse resultBean = new AjaxResponse();
+        resultBean.setIsok(false);
+        resultBean.setCode(e.getCode());
+        if(e.getCode() == CustomExceptionType.USER_INPUT_ERROR.getCode()){
+            resultBean.setMessage(e.getMessage());
+        }else if(e.getCode() == CustomExceptionType.SYSTEM_ERROR.getCode()){
+            resultBean.setMessage(e.getMessage() + ",系统出现异常，请联系管理员电话：1375610xxxx进行处理!");
+        }else{
+            resultBean.setMessage("系统出现未知异常，请联系管理员电话：13756108xxx进行处理!");
+        }
+        return resultBean;
     }
 
     public static AjaxResponse success() {
